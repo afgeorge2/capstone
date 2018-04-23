86ab0f5832ff683363a2348e0e8418ec9fc596cd
Mary Rachael Brown <maryrachaelbrown@gmail.com>
2018-04-23 16:04:39 -0400

    beer model

---
+++ Capstone.Web/Models/Beer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class Beer
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public double AlcoholByVolume { get; set; }
+        public string BeerType { get; set; }
+        public string BreweryName { get; set; }
+        public int BreweryId { get; set; }
+    }
+}
