32c294c99a16eeb159ac0936c8fba07e4ba43cc9
Adam George <afgeorge2@gmail.com>
2018-04-23 12:51:56 -0400

    commit

---
+++ Capstone.Web/Models/Registration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class Registration
+    {
+        
+        public string UserName { get; set; }
+       
+        public string Password { get; set; }
+        
+        public string EMailAddress { get; set; }
+    }
+}
